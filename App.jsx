5e68328e3c7102eac4b3e2fe8e54bec59512eeaf
Pulkit422 <143630962+Pulkit422@users.noreply.github.com>
2024-01-08 14:13:36 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import Imageslider from './components/imageslider'
+import './App.css'
+import { Sliderdata } from './components/sliderdata'
+
+function App() {
+
+  return <Imageslider slides={Sliderdata}/>
+}
+
+export default App
